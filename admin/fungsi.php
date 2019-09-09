2502448f7ae932a20b70d197bd967bd90f493668
nooravipp-ux <nooravipp9@gmail.com>
2019-09-09 23:31:07 +0700

    fix algorithm 2

--- admin/fungsi.php
+++ admin/fungsi.php
@@ -33,16 +33,16 @@ function answer($kode){
         echo "<a class='btn col-sm-1 mrg btn-lg btn-outline-light' href='hasil_bap.php?kode=363A'>Tidak</a>";
     }    
     if($kode=='T004'){
-        echo "<a class='btn col-sm-1 mrg btn-lg btn-outline-light' href='bap.php?kode=T005R'>Ya</a>";
-        echo "<a class='btn col-sm-1 mrg btn-lg btn-outline-light' href='hasil_bap.php?kode=362'>Tidak</a>";
+        echo "<a class='btn col-sm-1 mrg btn-lg btn-outline-light' href='hasil_bap.php?kode=362'>Ya</a>";
+        echo "<a class='btn col-sm-1 mrg btn-lg btn-outline-light' href='bap.php?kode=T005R'>Tidak</a>";
     }
     if($kode=='T005R'){
         echo "<a class='btn col-sm-1 mrg btn-lg btn-outline-light' href='bap.php?kode=T006'>Ya</a>";
-        echo "<a class='btn col-sm-1 mrg btn-lg btn-outline-light' href='bap.php?kode='>Tidak</a>";
+        echo "<a class='btn col-sm-1 mrg btn-lg btn-outline-light' href='hasil_bap.php?kode=362'>Tidak</a>";
     }
     if($kode=='T006'){
         echo "<a class='btn col-sm-1 mrg btn-lg btn-outline-light' href='hasil_bap.php?kode=363B'>Ya</a>";
-        echo "<a class='btn col-sm-1 mrg btn-lg btn-outline-light' href='bap.php?kode=T003'>Tidak</a>";
+        echo "<a class='btn col-sm-1 mrg btn-lg btn-outline-light' href='hasil_bap.php?kode=362'>Tidak</a>";
     }
 
 }
